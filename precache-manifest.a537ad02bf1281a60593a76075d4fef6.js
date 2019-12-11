ddfadd6598c1139e0a9561929635c958108eca5f
Rushiraj <rushibrahmbhatt33@gmail.com>
2019-12-11 14:03:55 +0530

    Updates

---
+++ precache-manifest.a537ad02bf1281a60593a76075d4fef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c3e4b53cbe3694597ee8a2a02e158a",
+    "url": "/contact-book-react/index.html"
+  },
+  {
+    "revision": "e3e5137c460382b41ff6",
+    "url": "/contact-book-react/static/css/main.b87b67a2.chunk.css"
+  },
+  {
+    "revision": "ae7674ace0a0fd7a141a",
+    "url": "/contact-book-react/static/js/2.fde863ad.chunk.js"
+  },
+  {
+    "revision": "e3e5137c460382b41ff6",
+    "url": "/contact-book-react/static/js/main.7bd5df84.chunk.js"
+  },
+  {
+    "revision": "97f89826d61782f60bf7",
+    "url": "/contact-book-react/static/js/runtime-main.51fb6355.js"
+  }
+]);
